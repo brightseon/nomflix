86888af222b8d2071a5af178d89905b66db82ae7
seon <seonu0823@gmail.com>
2019-02-24 23:21:20 +0900

    #2-1 React Router Part One

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Router from './Router';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-            </div>
+            <>
+                <Router />
+            </>
         );
     };
 };
---
+++ src/Components/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from 'Routes/Home';
+import Search from 'Routes/Search';
+import TV from 'Routes/TV';
+
+export default () => (
+    <Router>
+        <>
+            <Route path="/" exact component={ Home } />
+            <Route path="/tv" exact component={ TV } />
+            <Route path="/search" exact component={ Search } />
+        </>
+    </Router>
+);
