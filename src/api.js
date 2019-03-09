9864804272da5eff20f62510d319da92f17a406c
seon <seonu0823@gmail.com>
2019-03-09 23:03:27 +0900

    #4-2 API Verbs part One

--- src/api.js
+++ src/api.js
@@ -8,7 +8,14 @@ const api = axios.create({
     }
 });
 
-// #4-1 Sexy Networking with Axios Instances
-api.get('tv/popular');
+export const moviesApi = {
+    nowPlaying : () => api.get('movie/now_playing'),
+    upcoming : () => api.get('movie/upcoming'),
+    popular : () => api.get('movie/popular')
+};
 
-export default api;
+export const tvApi = {
+    topRated : () => api.get('tv/top_rated'),
+    popular : () => api.get('tv/popular'),
+    airingToday : () => api.get('tv/airing_today')
+};
