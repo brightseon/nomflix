255ecebb2ef106622e5dd6b0dd6fc6733d172038
seon <seonu0823@gmail.com>
2019-03-09 23:21:30 +0900

    #4-3 API Verbs part Two

--- src/api.js
+++ src/api.js
@@ -11,11 +11,33 @@ const api = axios.create({
 export const moviesApi = {
     nowPlaying : () => api.get('movie/now_playing'),
     upcoming : () => api.get('movie/upcoming'),
-    popular : () => api.get('movie/popular')
+    popular : () => api.get('movie/popular'),
+    movieDetail : id => api.get(`movie/${ id }`, {
+        params : {
+            append_to_response : 'videos'
+        }
+    }),
+    search : term => api.get('search/movie', {
+        params : {
+            // 어떤 파라미터 값을 넘기든 값을 인코딩하고, 그 문자열로 검색한다.
+            query : encodeURIComponent(term)
+        }
+    })
 };
 
 export const tvApi = {
     topRated : () => api.get('tv/top_rated'),
     popular : () => api.get('tv/popular'),
-    airingToday : () => api.get('tv/airing_today')
+    airingToday : () => api.get('tv/airing_today'),
+    showDetail : id => api.get(`tv/${ id }`, {
+        params : {
+            append_to_response : 'videos'
+        }
+    }),
+    search : term => api.get('search/tv', {
+        params : {
+            // 어떤 파라미터 값을 넘기든 값을 인코딩하고, 그 문자열로 검색한다.
+            query : encodeURIComponent(term)
+        }
+    })
 };
