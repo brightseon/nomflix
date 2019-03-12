8d37819a8be332a4ed0d04e0c4b43f6b41529980
seon <seonu0823@gmail.com>
2019-03-12 22:00:39 +0900

    #5-2 Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HomePresenter from './HomePresenter';
+import { moviesApi } from 'api';
 
 export default class extends Component {
     state = {
@@ -10,8 +11,33 @@ export default class extends Component {
         loading : true
     };
 
+    componentDidMount = async () => {
+        try {
+            const { data : { results : nowPlaying } } = await moviesApi.nowPlaying();
+            const { data : { results : upcoming } } = await moviesApi.upcoming();
+            const { data : { results : popular } } = await moviesApi.popular();
+            
+            this.setState({
+                nowPlaying,
+                upcoming,
+                popular
+            });
+        } catch(err) {
+            console.log('HomeContainer.js componentDidMount error : ', err);
+
+            this.setState({
+                error : 'Cant find movies information.'
+            });
+        } finally {
+            this.setState({
+                loading : false
+            });
+        }
+    };
+
     render() {
         const { nowPlaying, upcoming, popular, error, loading } = this.state;
+        console.log(this.state);
 
         return (
             <HomePresenter nowPlaying={ nowPlaying } upcoming={ upcoming } popular={ popular }
