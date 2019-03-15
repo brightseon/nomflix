72e9f1c8bd70aa33d4ac19be05e29c0942bebb62
seon <seonu0823@gmail.com>
2019-03-15 22:13:56 +0900

    #5-5 Detail Container part One

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -8,6 +8,17 @@ export default class extends Component {
         error : null
     };
 
+    componentDidMount = async () => {
+        const { match : { params : { id } }, history : { push } } = this.props;
+        const parsedId = parseInt(id);
+
+        if(isNaN(parsedId)) {
+            push('/');
+
+            return;
+        }
+    };
+
     render() {
         const { result, loading, error } = this.state;
 
