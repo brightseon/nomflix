25748637ab4112239a1096511a03519f176cc0bb
seon <seonu0823@gmail.com>
2019-03-18 21:29:19 +0900

    #6-0 Presenter Structure

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,16 @@
-export default () => 'Search';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SearchPresenter = ({ movieResults, tvResults, loading, error, handleSubmit }) => null;
+
+SearchPresenter.propTypes = {
+    movieResults : PropTypes.array,
+    tvResults : PropTypes.array,
+    loading : PropTypes.bool.isRequired,
+    error : PropTypes.string,
+    searchTerm : PropTypes.string,
+    handleSubmit : PropTypes.func.isRequired
+};
+
+export default SearchPresenter;
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,15 @@
-export default () => 'TV';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TVPresenter = ({ toRated, popular, airingToday, loading, error }) => null;
+
+TVPresenter.propTypes = {
+    toRated : PropTypes.array,
+    popular : PropTypes.array,
+    airingToday : PropTypes.array,
+    loading : PropTypes.bool.isRequired,
+    error : PropTypes.string
+};
+
+export default TVPresenter;
